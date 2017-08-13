f63a654a893fdd2293deb18b8b4759a1134d7857
Andy Ferris <andy.ferris@roames.com.au>
2017-08-13 22:34:42 +1000

    Introduced `AdjointArray` and `ConjAdjointArray`

--- base/linalg/conjarray.jl
+++ base/linalg/conjarray.jl
@@ -44,8 +44,8 @@ const ConjMatrix{T,M<:AbstractMatrix} = ConjArray{T,2,M}
 IndexStyle(::CA) where {CA<:ConjArray} = IndexStyle(parent_type(CA))
 IndexStyle(::Type{CA}) where {CA<:ConjArray} = IndexStyle(parent_type(CA))
 
-@propagate_inbounds getindex(a::ConjArray{T,N}, i::Int) where {T,N} = conj(getindex(a.parent, i))
-@propagate_inbounds getindex(a::ConjArray{T,N}, i::Vararg{Int,N}) where {T,N} = conj(getindex(a.parent, i...))
+@propagate_inbounds getindex(a::ConjArray{T,N}, i::Int) where {T,N} = conj(getindex(a.parent, i))::T
+@propagate_inbounds getindex(a::ConjArray{T,N}, i::Vararg{Int,N}) where {T,N} = conj(getindex(a.parent, i...))::T
 @propagate_inbounds setindex!(a::ConjArray{T,N}, v, i::Int) where {T,N} = setindex!(a.parent, conj(v), i)
 @propagate_inbounds setindex!(a::ConjArray{T,N}, v, i::Vararg{Int,N}) where {T,N} = setindex!(a.parent, conj(v), i...)
 
@@ -53,9 +53,134 @@ IndexStyle(::Type{CA}) where {CA<:ConjArray} = IndexStyle(parent_type(CA))
 
 # Currently, this is default behavior for RowVector only
 @inline conj(a::ConjArray) = parent(a)
+@inline adjoint(a::ConjArray) = ConjAdjointArray(parent(a))
 
 # Helper functions, currently used by RowVector
 @inline _conj(a::AbstractArray) = ConjArray(a)
 @inline _conj(a::AbstractArray{T}) where {T<:Real} = a
 @inline _conj(a::ConjArray) = parent(a)
 @inline _conj(a::ConjArray{T}) where {T<:Real} = parent(a)
+
+"""
+AdjointArray(array)
+
+A lazy-view wrapper of an `AbstractArray`, taking the elementwise adjoint. This
+type is usually constructed (and unwrapped) via the [`adjoint`](@ref) function, but 
+currently this is the default behavior for `RowVector` only. For other arrays, the 
+`AdjointArray` constructor can be used directly.
+
+# Examples
+```jldoctest
+julia> [1+im, 1-im]'
+1×2 RowVector{Complex{Int64},AdjointArray{Complex{Int64},1,Array{Complex{Int64},1}}}:
+1-1im  1+1im
+
+julia> AdjointArray([1+im 0; 0 1-im])
+2×2 AdjointArray{Complex{Int64},2,Array{Complex{Int64},2}}:
+1-1im  0+0im
+0+0im  1+1im
+```
+"""
+struct AdjointArray{T,N,A<:AbstractArray} <: AbstractArray{T,N}
+    parent::A
+end
+
+@inline AdjointArray(a::AbstractArray{T,N}) where {T,N} = AdjointArray{adjoint_type(T),N,typeof(a)}(a)
+
+const AdjointVector{T,V<:AbstractVector} = AdjointArray{T,1,V}
+@inline AdjointVector(v::AbstractVector{T}) where {T} = AdjointArray{adjoint_type(T),1,typeof(v)}(v)
+
+const AdjointMatrix{T,M<:AbstractMatrix} = AdjointArray{T,2,M}
+@inline AdjointMatrix(m::AbstractMatrix{T}) where {T} = AdjointArray{adjoint_type(T),2,typeof(m)}(m)
+
+# This type can cause the element type to change under conjugation - e.g. an array of complex arrays.
+@inline adjoint_type(x) = conj_type(typeof(x))
+@inline adjoint_type(::Type{T}) where {T} = promote_op(adjoint, T)
+
+@inline parent(c::AdjointArray) = c.parent
+@inline parent_type(c::AdjointArray) = parent_type(typeof(c))
+@inline parent_type(::Type{AdjointArray{T,N,A}}) where {T,N,A} = A
+
+@inline size(a::AdjointArray) = size(a.parent)
+IndexStyle(::AA) where {AA<:AdjointArray} = IndexStyle(parent_type(AA))
+IndexStyle(::Type{AA}) where {AA<:AdjointArray} = IndexStyle(parent_type(AA))
+
+@propagate_inbounds getindex(a::AdjointArray{T,N}, i::Int) where {T,N} = adjoint(getindex(a.parent, i))::T
+@propagate_inbounds getindex(a::AdjointArray{T,N}, i::Vararg{Int,N}) where {T,N} = adjoint(getindex(a.parent, i...))::T
+@propagate_inbounds setindex!(a::AdjointArray{T,N}, v, i::Int) where {T,N} = setindex!(a.parent, adjoint(v), i)
+@propagate_inbounds setindex!(a::AdjointArray{T,N}, v, i::Vararg{Int,N}) where {T,N} = setindex!(a.parent, adjoint(v), i...)
+
+@inline similar(a::AdjointArray, ::Type{T}, dims::Dims{N}) where {T,N} = similar(parent(a), T, dims)
+
+# Currently, this is default behavior for RowVector only
+@inline adjoint(a::AdjointArray) = parent(a)
+@inline conj(a::AdjointArray) = ConjAdjointArray(parent(a))
+
+# Helper functions, currently used by RowVector
+@inline _adjoint(a::AbstractArray) = AdjointArray(a)
+@inline _adjoint(a::AbstractArray{T}) where {T<:Real} = a
+@inline _adjoint(a::AbstractArray{T}) where {T<:Number} = conj(a)
+@inline _adjoint(a::AdjointArray) = parent(a)
+@inline _adjoint(a::AdjointArray{T}) where {T<:Real} = parent(a)
+@inline _adjoint(a::AdjointArray{T}) where {T<:Number} = parent(a)
+
+"""
+ConjAdjointArray(array)
+
+A lazy-view wrapper of an `AbstractArray`, mapping each element `i` to `conj(adjoint(i))`.
+This type is usually constructed (and unwrapped) via consecutive [`adjoint`](@ref) and
+[`conj`](@ref) functions, but currently this is the default behavior for `RowVector` only.
+For other arrays, the `ConjAdjointArray` constructor can be used directly.
+
+# Examples
+```jldoctest
+julia> conj([1+im, 1-im]')
+1×2 RowVector{Complex{Int64},ConjAdjointArray{Complex{Int64},1,Array{Complex{Int64},1}}}:
+1+1im  1-1im
+
+julia> ConjAdjointArray([1+im 0; 0 1-im])
+2×2 ConjAdjointArray{Complex{Int64},2,Array{Complex{Int64},2}}:
+1+1im  0+0im
+0+0im  1-1im
+```
+"""
+struct ConjAdjointArray{T,N,A<:AbstractArray} <: AbstractArray{T,N}
+    parent::A
+end
+
+@inline ConjAdjointArray(a::AbstractArray{T,N}) where {T,N} = ConjAdjointArray{conjadjoint_type(T),N,typeof(a)}(a)
+
+const ConjAdjointVector{T,V<:AbstractVector} = ConjAdjointArray{T,1,V}
+@inline ConjAdjointVector(v::AbstractVector{T}) where {T} = ConjAdjointArray{conjadjoint_type(T),1,typeof(v)}(v)
+
+const ConjAdjointMatrix{T,M<:AbstractMatrix} = ConjAdjointArray{T,2,M}
+@inline ConjAdjointMatrix(m::AbstractMatrix{T}) where {T} = ConjAdjointArray{conjadjoint_type(T),2,typeof(m)}(m)
+
+# This type can cause the element type to change under conjugation - e.g. an array of complex arrays.
+@inline conjadjoint_type(x) = conjadjoint_type(typeof(x))
+@inline conjadjoint_type(::Type{T}) where {T} = promote_op(x -> conj(adjoint(x)), T)
+
+@inline parent(c::ConjAdjointArray) = c.parent
+@inline parent_type(c::ConjAdjointArray) = parent_type(typeof(c))
+@inline parent_type(::Type{ConjAdjointArray{T,N,A}}) where {T,N,A} = A
+
+@inline size(a::ConjAdjointArray) = size(a.parent)
+IndexStyle(::A) where {A<:ConjAdjointArray} = IndexStyle(parent_type(A))
+IndexStyle(::Type{A}) where {A<:ConjAdjointArray} = IndexStyle(parent_type(A))
+
+@propagate_inbounds getindex(a::ConjAdjointArray{T,N}, i::Int) where {T,N} = conj(adjoint(getindex(a.parent, i)))::T
+@propagate_inbounds getindex(a::ConjAdjointArray{T,N}, i::Vararg{Int,N}) where {T,N} = conj(adjoint(getindex(a.parent, i...)))::T
+@propagate_inbounds setindex!(a::ConjAdjointArray{T,N}, v, i::Int) where {T,N} = setindex!(a.parent, conj(adjoint(v)), i)
+@propagate_inbounds setindex!(a::ConjAdjointArray{T,N}, v, i::Vararg{Int,N}) where {T,N} = setindex!(a.parent, conj(adjoint(v)), i...)
+
+@inline similar(a::ConjAdjointArray, ::Type{T}, dims::Dims{N}) where {T,N} = similar(parent(a), T, dims)
+
+# Currently, this is default behavior for RowVector only
+@inline adjoint(a::ConjAdjointArray) = ConjArray(parent(a))
+@inline conj(a::ConjAdjointArray) = AdjointArray(parent(a))
+
+# Helper functions, currently used by RowVector
+@inline _conjadjoint(a::AbstractArray) = ConjAdjointArray(a)
+@inline _conjadjoint(a::AbstractArray{T}) where {T<:Number} = conj(a)
+@inline _conjadjoint(a::AdjointArray) = parent(a)
+@inline _conjadjoint(a::ConjAdjointArray{T}) where {T<:Number} = parent(a)
